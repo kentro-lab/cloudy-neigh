44584ed5f22f5e6508ce66b45852d65e80b63fb5
Ali Amiri <ali@shooka-mac.local>
2023-10-18 14:20:14 -0700

    Setup Bazel and Gazelle

---
+++ cmd/test_go/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	_ "github.com/cockroachdb/errors/errorspb"
+	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/vfs"
+	"github.com/kentro-lab/cloudy-neigh/proto/apipb"
+)
+
+func main() {
+	db, err := pebble.Open("", &pebble.Options{FS: vfs.NewMem()})
+	if err != nil {
+		log.Fatal(err)
+	}
+	key := []byte("hello")
+	if err := db.Set(key, []byte("world"), pebble.Sync); err != nil {
+		log.Fatal(err)
+	}
+	value, closer, err := db.Get(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s %s\n", key, value)
+	if err := closer.Close(); err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+	msg := apipb.TestMessage{}
+	fmt.Println("Message: ", msg)
+
+}
